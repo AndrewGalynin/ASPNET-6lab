f84a53eab0ef430b8868a372445313ee51b34e27
AndrewGalynin <galynin69@gmail.com>
2023-10-15 17:02:35 +0300

    asp.net 6 lab
    asp.net 6 lab

---
+++ lab 6/Controllers/OrderControllers.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaOrder.Models;
+using System.Collections.Generic;
+
+namespace PizzaOrder.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (user.Age > 16)
+            {
+                return View("ProductForm", user);
+            }
+            else
+            {
+                return View("OrderSummary", new List<Product>());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ProductForm(List<Product> products)
+        {
+            return View("OrderSummary", products);
+        }
+    }
+}
+
+
---
+++ lab 6/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PizzaOrder.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
